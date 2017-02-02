5a107cee5334915fcce82539e286eedcc68af874
0x00-0x00 <andre.marques@fatec.sp.gov.br>
2017-02-02 13:24:48 -0200

    Improved code

--- fuzz.pl
+++ fuzz.pl
@@ -1,50 +1,94 @@
 #!/usr/bin/env perl
 
-
-use Scalar::Util;
+#use strict;
+#use warnings;
 use IO::Socket;
 
 sub generateJunk {
-    srand time;
-    $n = scalar(@_);
-    if($n < 1) {
+    my $n = scalar(@_);
+    if($n < 2) {
         return 0;
     }
-    my $size = @_[0];
+    my $size = $_[0];
+    my $seed = $_[1];
+    srand unpack("L", $seed);
     my @char = ('A'..'Z', 'a'..'z', 0..9, qw(! @ # $ % ^ & * + - = ));
     my $junk = join("", @char[map{ rand @char } (1..$size) ]);
-    printf "[+] Generated %d bytes of junk.\n", length $junk;
     return $junk;
 }
 
 
 # Script to send junk input to a listening socket
 my $act = 0;
-my $server = '127.0.0.1';
-my $port = 8888;
+print "[-] Type the target IP address: ";
+my $server = <STDIN>;
+chomp $server;
 
-print "Type how many iterations of fuzzing do you wish to do: ";
+print "[-] Type the target port: ";
+my $port = <STDIN>;
+chomp $port;
+
+print "[-] Type how many iterations of fuzzing do you wish to do: ";
 my $input = <STDIN>;
-my $valid_input = 0;
 chomp $input;
 
+print "[-] Type how many time of interval between requests: ";
+my $interval = <STDIN>;
+chomp $interval;
 
-for(my $i = 0; $i < $input; $i++) {
-    printf "Iteration #%d\n", $i;
-    if($socks = IO::Socket::INET->new(
-            Proto=>"tcp",
-            PeerAddr=>$server,
-            PeerPort=>$port,
-            TimeOut=>5,
-        )) {
-        my $bufflen = rand(4096);
-        my $blob_size = $bufflen;
-        my $buffer = generateJunk($bufflen);
-        my $junk = pack "ssa128", $act, $blob_size, $buffer;
-        printf "Sending buffer to port $port (%d bytes)\n", length $junk;
-        $socks->send($buffer);
-        sleep 1;
+print "\n[*] Fuzzing session with $input cycles set to destination $server:$port\n";
+
+# Create a socket
+my $socks = IO::Socket::INET->new(
+        Proto=> "tcp",
+        PeerAddr=> $server,
+        PeerPort=> $port,
+        TimeOut=>5,
+        Reuse=>1   
+);
+
+
+my $seed;
+my $randSeed;
+open(RAND, "/dev/urandom");
+read(RAND, $seed, 4);
+srand(unpack("L", $seed));
+
+for(my $i = 0; $i < int($input); $i++) {
+    
+    my $socks = IO::Socket::INET->new(
+        Proto=> "tcp",
+        PeerAddr=> $server,
+        PeerPort=> $port,
+        TimeOut=>5,
+        #Reuse=>1
+    );
+    
+    if(not $socks) {
+        print "Can't create no more sockets.\n";
+        exit;
     }
+
+    my $bufflen = rand(4096);
+    my $blob_size = $bufflen;
+    read(RAND, $randSeed, 4);
+    my $buffer = generateJunk($bufflen, $randSeed);
+
+    if($i % 2 == 0 ) {
+        read(RAND, $buffer, $bufflen);
+    }
+    my $packTemplate = sprintf "ssa%d", $bufflen;
+    my $junk = pack $packTemplate, $act, $blob_size, $buffer;
+    printf "Sending junk data to $server:$port (%d bytes)\n", length $junk;
+    $socks->send($buffer) if $socks;
+    #Close sockets
+    shutdown($socks, 2) if $socks;
+    close($socks) if $socks;
+    sleep $interval;
 }
 
+print "[*] Fuzzing complete.";
+
+
+ 
 
